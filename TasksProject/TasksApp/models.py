d0754dc3c5b8b854b498c1220fe296da015d19f0
Devora Wexler <devora.video@gmail.com>
2025-12-30 14:03:20 +0200

    1

--- TasksProject/TasksApp/models.py
+++ TasksProject/TasksApp/models.py
@@ -4,10 +4,9 @@ from django.contrib.auth.models import AbstractUser
 
 # User
 class User(AbstractUser):
-    phone_number = models.CharField(max_length=15, blank=True, null=True)
-    date_of_birth = models.DateField(blank=True, null=True)
-    address = models.TextField(blank=True, null=True)
-
+    name = models.CharField(max_length=150)
+    password = models.CharField(max_length=128)
+    email = models.EmailField(unique=True)
     def __str__(self):
         return self.username
 
